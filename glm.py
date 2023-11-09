f47a760cd55e22aad1a6cde037dddabaaea95999
Daniel Sun <sun.daniel@outlook.com>
2023-11-09 05:59:59 +0000

    update exports

--- glm.py
+++ glm.py
@@ -6,7 +6,8 @@ workspace = jmake.Workspace("glm")
 glm = jmake.Project("glm", jmake.Target.HEADER_LIBRARY)
 files = jmake.glob('glm', ['**/*.hpp', '**/*.h', '**/*.inl'])
 glm.add(jmake.fullpath(files))
-glm.includes.extend(jmake.fullpath('glm'))
+
+glm.export(includes=jmake.fullpath('glm'))
 
 workspace.add(glm)
 jmake.generate(workspace)
