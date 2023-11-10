a4ce623a312e237207a8e267aedb85523f8504c2
Daniel Sun <sun.daniel@outlook.com>
2023-11-10 16:26:11 +0000

    fix include path

--- glm.py
+++ glm.py
@@ -7,5 +7,5 @@ glm = jmake.Project("glm", jmake.Target.HEADER_LIBRARY)
 files = jmake.glob('glm', ['**/*.hpp', '**/*.h', '**/*.inl'])
 glm.add(jmake.fullpath(files))
 
-glm.export(includes=jmake.fullpath('glm'))
+glm.export(includes=jmake.fullpath('.'))
 workspace.add(glm)
