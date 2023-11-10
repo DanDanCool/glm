bab40c6f5520bcbbb390f66182ac3da5e53b811b
Daniel Sun <sun.daniel@outlook.com>
2023-11-10 21:51:59 +0000

    jmake.glob fixes

--- glm.py
+++ glm.py
@@ -5,7 +5,7 @@ jmake.setupenv()
 workspace = jmake.Workspace("glm")
 glm = jmake.Project("glm", jmake.Target.HEADER_LIBRARY)
 files = jmake.glob('glm', ['**/*.hpp', '**/*.h', '**/*.inl'])
-glm.add(jmake.fullpath(files))
+glm.add(files)
 
 glm.export(includes=jmake.fullpath('.'))
 workspace.add(glm)
